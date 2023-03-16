a6121605e267b545adf25a083f9c23b86a74a326
trdung1999 <trungdungnguyen199@gmail.com>
2023-03-16 21:33:47 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+// Task 1. Tạo route "/" từ HomeController và hàm index 
+// Trỏ đến welcome.blade.php kèm biến pageTitle: "Trang chủ"
+
+
+
+// Task 2. Tạo route có dạng "/user/{name}" từ UserController và hàm show
+// Không sử dụng Model binding do param truyền vào là name
+
+
+
+// Task 3: Tạo route có dạng "/about" từ controller bất kỳ(có thể dùng HomeController)
+// Trỏ đến resources/views/pages/about.blade.php và đặt route name là about
